1784cea7e5c9c59c5679d90b38bbe4d18414c478
Alex Ruiz <Alex.ruiz-estradera@mytheresa.com>
2020-08-04 22:34:40 +0200

    Update generate-screenshots.js

--- utils/generate-screenshots.js
+++ utils/generate-screenshots.js
@@ -11,7 +11,7 @@ urls.forEach(async (url) => {
     });
     await page.screenshot({
         fullPage: true,
-        path: '/validator/screenshots/' + url.replace(/^.*\/\/[^\/]+/, '').replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, '_').replace(/(\.html)|(\.htm)|(\.php)/, '') + '.png'
+        path: '/validator/screenshots/' + url.replace(/^.*\/\/[^\/]+/, '').replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, '_').replace(/(\.html)|(\.htm)|(\.php)/, '').replace(/\\/g, '') + '.png'
     });
     await browser.close();
 });
