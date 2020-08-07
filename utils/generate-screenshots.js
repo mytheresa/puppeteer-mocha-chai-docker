2f034219b23603e2a2746e58fae312766521d309
Alex Ruiz Estradera <alex.ruiz-estradera@mytheresa.com>
2020-08-07 16:21:25 +0200

    Fix screenshots

--- utils/generate-screenshots.js
+++ utils/generate-screenshots.js
@@ -2,14 +2,29 @@ const puppeteer = require('puppeteer');
 const urls = process.env.URLS.split(',');
 
 const cookies = [{
-  'name': 'myth_cookie_policy',
-  'value': '1'
+  'name'     : 'myth_cookie_policy',   /* required property */
+  'value'    : '1',  /* required property */
+  'domain'   : 'www.mytheresa.com',
+  'path'     : '/',                /* required property */
+  'httponly' : false,
+  'secure'   : false,
+  'expires'  : (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
 },{
-  'name': 'TC_PRIVACY',
-  'value': '0@005@1%2C2%2C3@'
+  'name'     : 'TC_PRIVACY',   /* required property */
+  'value'    : '0@005@1%2C2%2C3@',  /* required property */
+  'domain'   : 'www.mytheresa.com',
+  'path'     : '/',                /* required property */
+  'httponly' : false,
+  'secure'   : false,
+  'expires'  : (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
 },{
-  'name': 'TC_PRIVACY_CENTER',
-  'value': '1%2C2%2C3'
+  'name'     : 'TC_PRIVACY_CENTER',   /* required property */
+  'value'    : '1%2C2%2C3',  /* required property */
+  'domain'   : 'www.mytheresa.com',
+  'path'     : '/',                /* required property */
+  'httponly' : false,
+  'secure'   : false,
+  'expires'  : (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
 }];
 
 urls.forEach(async (url) => {
@@ -17,17 +32,14 @@ urls.forEach(async (url) => {
     const page = await browser.newPage();
 
     await page.setViewport({width: 1280, height: 720});
-    await page.goto(url, {'waitUntil': 'networkidle0'}).then(()=>{
-        console.log('Generating screeshot for ' + url);
-    });
     await page.setCookie(...cookies);
     const cookiesSet = await page.cookies(url);
     console.log(JSON.stringify(cookiesSet));
-    const cookieAcceptBtn = await page.$("button.btn-all");
-    if (cookieAcceptBtn != null) {
-      cookieAcceptBtn.click()
-      await page.waitFor(1000)
-    }
+    await page.goto(url, {'waitUntil': ['load','domcontentloaded','networkidle0','networkidle2']}).then(()=>{
+        console.log('Generating screeshot for ' + url);
+    });
+
+
     await page.screenshot({
         fullPage: true,
         path: '/validator/screenshots/' + url.replace(/^.*\/\/[^\/]+/, '').replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, '_').replace(/(\.html)|(\.htm)|(\.php)/, '').substr(1) + '.png'
